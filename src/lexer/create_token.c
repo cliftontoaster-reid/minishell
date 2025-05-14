5da509a5f6c4328b933f17abac0aebd32bb07317
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-05-14 16:41:28 +0200

    Implement create_token function for token creation in lexer

---
+++ src/lexer/create_token.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   create_token.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 15:07:52 by lfiorell@st       #+#    #+#             */
+/*   Updated: 2025/05/14 16:41:11 by lfiorell@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "lexer.h"
+#include "libft.h"
+#include <stdlib.h>
+
+t_token	*create_token(char *value, t_token_type type)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->value = ft_strdup(value);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->type = type;
+	return (token);
+}
