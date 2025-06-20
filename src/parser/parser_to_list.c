7eb6006b585c3e966f4696ab6d63e4457b323ad1
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-06-20 14:10:19 +0200

    I need to see what has changed in the workspace to generate a commit message. feat(git): add conventional commit message template
    • Create .gitmessage to enforce Conventional Commits standard
    • Template includes format, types, and a detailed example
    • Aims to improve commit history clarity and consistency

--- src/parser/parser_to_list.c
+++ src/parser/parser_to_list.c
@@ -6,13 +6,14 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 15:38:30 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/06/19 15:40:55 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/06/20 14:05:19 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "parser.h"
 #include <errno.h>
+#include <unistd.h>
 
 t_cmd	*parser_to_list(t_parser *parser)
 {
@@ -46,6 +47,15 @@ t_cmd	*parser_to_list(t_parser *parser)
 		current = current->next;
 		i++;
 	}
+	commands[i].args = NULL;
+	commands[i].argc = 0;
+	commands[i].is_pipe = false;
+	commands[i].fd_infile = STDIN_FILENO;
+	commands[i].fd_outfile = STDOUT_FILENO;
+	commands[i].redirect_in = NULL;
+	commands[i].redirect_out = NULL;
+	commands[i].redirect_append = NULL;
+	commands[i].redirect_heredoc = NULL;
 	if (i < len)
 	{
 		free(commands);
