9c69957979df029e769fd93f094e3c1aa0dd740f
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-06-23 11:10:05 +0200

    fix(main): update prompt and improve parser/lexer resource management

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell@student.42nice.fr <lfiorell>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 14:49:10 by lfiorell@st       #+#    #+#             */
-/*   Updated: 2025/06/20 15:29:08 by lfiorell@st      ###   ########.fr       */
+/*   Updated: 2025/06/23 11:09:08 by lfiorell@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define PROMPT "nanoshell> "
+#define PROMPT "picoshell> "
 #define BOLD "\033[1m"
 #define RESET "\033[0m"
 
@@ -95,7 +95,7 @@ int	main(int argc, char **argv, char **envp)
 	t_reader		*reader_ptr;
 	char			*cached_input;
 	t_linereader	reader;
-			t_cmd *commands;
+	t_cmd			*commands;
 
 	ft_bzero(&reader, sizeof(t_linereader));
 	env_list = b_fromenvp(envp);
@@ -131,9 +131,14 @@ int	main(int argc, char **argv, char **envp)
 			print_parser(reader_ptr->parser);
 			commands = parser_to_list(reader_ptr->parser);
 			ft_pipex(commands, reader_ptr->env);
+			parser_free(reader_ptr->parser);
+			reader_ptr->parser = NULL;
 		}
-		free_lexer(reader_ptr->lexer);
-		parser_free(reader_ptr->parser);
+		if (reader_ptr->lexer)
+		{
+			free_lexer(reader_ptr->lexer);
+			reader_ptr->lexer = NULL;
+		}
 		reader_ptr->tokens = NULL;
 	}
 	// free reader resources
